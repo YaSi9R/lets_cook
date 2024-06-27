5bde51b1aeea3c973d3bec76428e15efbfe066c8
Yasir <Yasir@gmail.com>
2024-06-27 15:58:59 +0530

    first commit

---
+++ react-app/src/App.js
@@ -0,0 +1,44 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import './App.css';
+import Header from './components/Header';
+
+// import Home from "./pages/Home";
+
+// import Consulting from "./pages/Consulting";
+// import Contactus from "./pages/Contactus";
+// import Design from "./pages/Design";
+// import Development from "./pages/Development";
+// import Marketing from "./pages/Marketing";
+// import Products from "./pages/Products";
+// import Services from "./pages/Services";
+// import Signup from "./pages/Signup";
+
+function App() {
+  return (
+    <>
+      <Router>
+        <section
+          className="website h-screen "
+          >
+            <Header/>
+         
+          {/* <Section/>
+          <div className="flex flex-col justify-center text-center items-center h-3/4">
+            <h2 className="text-black text-2xl font-medium ">My major Project</h2>
+            <h1 className="md:text-5xl text-3xl text-black font-semibold py-5">
+             These Are the Books for All ages
+            </h1>
+            <div className="text-xl">
+            </div>
+          </div>
+          <Section/> */}
+        </section>
+
+       
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ react-app/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+const Header = () => {
+    // const [open, setOpen] = useState(false);
+    return (
+        <nav className="bg-blue">
+      {/* <div className="flex items-center font-medium justify-around">
+        <div className="z-50 p-5  w-full flex justify-between text-center "> */}
+
+<h1>Hello we want to make a lets cook application for providing recipes of all foods</h1>
+
+        {/* </div>
+        </div> */}
+    </nav>
+  );
+};
+
+export default Header;
