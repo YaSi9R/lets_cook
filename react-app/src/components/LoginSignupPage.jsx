093990ce51e2d2e622e378bb158c031b8e40a6dd
adityam2003 <166909570+adityam2003@users.noreply.github.com>
2024-07-04 12:36:52 +0530

    uploading loginsignuppage

---
+++ react-app/src/components/LoginSignupPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import LoginSignupForm from './loginsignupform/LoginSignupForm';
+
+const LoginSignupPage = () => {
+  return (
+    <div className="login-signup-page">
+      <h1>Login/Signup</h1>
+      <LoginSignupForm />
+    </div>
+  );
+};
+
+export default LoginSignupPage;
+
